e6cb3fff87961364edf7b760339f215639e7f957
aliilhan <75568951+aliilhangis@users.noreply.github.com>
2025-01-18 13:31:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import random
-import time
+from telegram.ext import Application, CommandHandler
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import ContextTypes
 
-# Bot Token'ınızı buraya girin
+# Bot Token
 TOKEN = '7528327308:AAG5VxRb9QqArCLeP3gDxtJSE-m_jOV11Ho'
 
-# İsim listesi
+# Oyun listesi
 games = [
     "Gates of Olympus", "The Dog House", "Sweet Bonanza", "John Hunter and the Tomb of the Scarab Queen", 
     "Wolf Gold", "Great Rhino Megaways", "Big Bass Bonanza", "Fruit Party", "Release the Kraken", 
@@ -14,38 +14,36 @@ games = [
     "The Dog House Megaways"
 ]
 
-# 85 ile 98 arasında rastgele oran seçme fonksiyonu
+# Rastgele yüzde seçme fonksiyonu
 def random_percentage():
     return random.uniform(85, 98)
 
+# Start komutu işleyicisi
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot çalışıyor. /news komutunu kullanarak haber alabilirsiniz.")
+
 # Haber gönderme fonksiyonu
-def send_news(update: Update, context: CallbackContext):
-    # Saat başı 5 rastgele oyun ve oran seçme
+async def send_news(update: Update, context: ContextTypes.DEFAULT_TYPE):
     selected_games = random.sample(games, 5)
     news_message = "Saat Başına Seçilen Oyunlar ve Oranlar:\n\n"
     
     for game in selected_games:
         news_message += f"{game}: {random_percentage():.2f}%\n"
     
-    update.message.reply_text(news_message)
+    await update.message.reply_text(news_message)
 
-# Start komutunun işlevi
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text("Bot çalışıyor. /news komutunu kullanarak haber alabilirsiniz.")
-
-# Main fonksiyonu
-def main():
-    # Updater ve Dispatcher'ı başlatma
-    updater = Updater(TOKEN)
-    dispatcher = updater.dispatcher
-
-    # Komutları ekleyelim
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("news", send_news))
-
-    # Botu çalıştırma
-    updater.start_polling()
-    updater.idle()
+# Ana fonksiyon
+async def main():
+    # Uygulamayı başlat
+    application = Application.builder().token(TOKEN).build()
+    
+    # Komut işleyicilerini ekle
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("news", send_news))
+    
+    # Botu başlat
+    await application.run_polling()
 
 if __name__ == '__main__':
-    main()
+    import asyncio
+    asyncio.run(main())
