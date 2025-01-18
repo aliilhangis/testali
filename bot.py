ee1762ae85a740b3d4265a3cc82801d5b4f484bf
aliilhan <75568951+aliilhangis@users.noreply.github.com>
2025-01-18 13:17:05 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import random
+from telegram.ext import Application
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from apscheduler.schedulers.background import BackgroundScheduler
